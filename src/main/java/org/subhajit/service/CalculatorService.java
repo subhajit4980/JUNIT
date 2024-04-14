360491a36f6c56b3124e0222c036751564185731
subhajitpatra498 <subhajitpatra498@gmail.com>
2024-04-14 21:12:53 +0530

    add service and update junit 5

--- src/main/java/org/subhajit/service/CalculatorService.java
+++ src/main/java/org/subhajit/service/CalculatorService.java
@@ -1,19 +1,19 @@
 package org.subhajit.service;
 
 public class CalculatorService {
-    public  int addNumber(int a ,int b)
+    public static   int addNumber(int a ,int b)
     {
         return a + b;
     }
-    public int product(int a ,int b)
+    public static int product(int a ,int b)
     {
         return a * b;
     }
-    public int divide(int a ,int b)
+    public static int divide(int a ,int b)
     {
         return a / b;
     }
-    public  int sumOfSomeNumbers(int ... numbers){
+    public static int sumOfSomeNumbers(int ... numbers){
         int sum=0;
         for(int i:numbers)
         {
--- src/test/java/org/subhajit/Service/CalculatorServiceTest.java
+++ src/test/java/org/subhajit/Service/CalculatorServiceTest.java
@@ -1,4 +1,55 @@
 package org.subhajit.Service;
 
+import org.junit.jupiter.api.*;
+import org.subhajit.service.CalculatorService;
+
 public class CalculatorServiceTest {
+    @BeforeAll
+    public static void init(){
+        System.out.println("starting point of all test cases");
+    }
+    @BeforeEach
+    public void beforeEach()
+    {
+        System.out.println("before test case");
+    }
+    @AfterEach
+    public void afterEach(){
+        System.out.println("after test case");
+    }
+    @Test
+    public void divideTest()
+    {
+        System.out.println("1st test case");
+        int sum=CalculatorService.divide(44,22);
+        int expected=2;
+        Assertions.assertEquals(expected,sum,"test fails");
+    }
+    @Test
+    public void addNumberTest()
+    {
+        System.out.println("2nd test case");
+        int sum=CalculatorService.addNumber(12,22);
+        int expected=34;
+        Assertions.assertEquals(expected,sum,"test fails");
+    }
+    @Test
+    public void productTest(){
+        System.out.println("3rd test case");
+        int product=CalculatorService.product(2,22);
+        int expected=44;
+        Assertions.assertEquals(expected,product,"test fails");
+    }
+    @Test
+    public void sumOfSomeNumbersTest()
+    {
+        System.out.println("4th test case");
+        int sum=CalculatorService.sumOfSomeNumbers(12,2,3,4,5);
+        int expected=26;
+        Assertions.assertEquals(expected,sum,"test fails");
+    }
+    @AfterAll
+    public static void cleanup(){
+        System.out.println("ending of all test cases");
+    }
 }
